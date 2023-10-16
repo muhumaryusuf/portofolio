1ff420b21a2e5ac3710d852e8a9ccb62f2f190ce
muhammadumaryusuf <therapper603@gmail.com>
2023-10-16 14:15:54 +0700

    add contact

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ function toggleDetail(e) {
     console.log($(item).children(".about-exp-item-detail"))
 
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("Isi emailnu cukka ulu")
+    } else if(!$(subject).val()) {
+        alert("Isi subject latai")
+    } else if(!$(message).val()) {
+        alert("aga pesannu? degaga?")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
